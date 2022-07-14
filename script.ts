1fc9b6e4aad10a7735c2af3e48647cafd5545817
Alysson Montovanelli <77108174+alyssonmontovanelli@users.noreply.github.com>
2022-07-14 14:12:02 -0300

    Add files via upload

---
+++ script.ts
@@ -0,0 +1,92 @@
+interface Veiculo{
+ nome: string;
+ placa: string;
+ entrada: Date;
+}
+
+
+(function () {
+ const $ = (seletor: string): HTMLInputElement | null => document.querySelector(seletor);
+ 
+ function calcTempo(mil: number){
+  const min = Math.floor(mil/60000);
+  const sec = Math.floor((mil%60000)/ 1000);
+  return `${min} minutos e ${sec} segundos`
+ }
+
+ function patio(){
+  function ler(): Veiculo[]{
+   return localStorage.patio ? JSON.parse(localStorage.patio) : [];
+  }
+
+  function salvar(veiculo: Veiculo[]){
+   localStorage.setItem("patio", JSON.stringify(veiculo))
+  }
+
+
+  function adicionar(veiculo: Veiculo, salva?: boolean){
+   const linhas = document.createElement("tr")
+   linhas.innerHTML = `
+   <td>${veiculo.nome}</td>
+   <td>${veiculo.placa}</td>
+   <td>${veiculo.entrada}</td>
+   <td>
+    <button class="delete" data-placa="${veiculo.placa}">x</button>
+   </td>
+   `;   
+   linhas.querySelector(".delete")?.addEventListener("click", function(){
+    remover(this.dataset.placa)
+   })
+
+   $("#patio")?.appendChild(linhas);
+   
+   if (salva) salvar([...ler(), veiculo])
+  }
+
+  function remover(placa: string){
+   const {entrada, nome} = ler().find(veiculo => veiculo.placa === placa)
+
+   const tempo = calcTempo(new Date().getTime() - new Date(entrada).getTime())
+
+   if(confirm(`O veículo ${nome} permaneceu por ${tempo}. Deseja encerrar?`)) return;
+   salvar(ler().filter((veiculo) => veiculo.placa !== placa));
+   render()
+
+
+  }
+  
+  function render(){
+   $("#patio")!.innerHTML = "";
+   const patio = ler();
+
+   if(patio.length){
+    patio.forEach((veiculo) => adicionar(veiculo))
+   }
+
+  }
+
+  return{ler, adicionar, remover, salvar, render};
+ }
+
+
+ patio().render();
+ $("#cadastrar")?.addEventListener("click",()=>{
+  const nome = $("#nome")?.value;
+  const placa = $("#placa")?.value;
+
+  if(!nome || !placa){
+   alert("Os campos de nome e placa são obrigatórios")
+   return;
+  }
+  patio().adicionar({nome, placa, entrada: new Date}, true)
+ })
+
+
+
+
+
+
+
+
+
+}());
